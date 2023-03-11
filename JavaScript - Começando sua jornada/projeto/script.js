bedc19fe4ee35249fba42262c36a29499558af13
Carlos Eduardo Correia de Souza <CarlosCorreia1002@gmail.com>
2023-03-11 18:37:20 -0300

    Seção 1: Finalizada
    Seção 1: Iniciando estudos(FINALIZADA).

--- JavaScript - Começando sua jornada/projeto/script.js
+++ JavaScript - Começando sua jornada/projeto/script.js
@@ -160,15 +160,177 @@
 //###################################################################################################################################################################################
 //MANIPULANDO ARRAYS
 
-var lista = ['Carlos', 'Natalia', 'Maria', 2023]; //criado uma lista(array) com alguns dados
-console.log(lista.indexOf('Carlos')); //O método indexOf() retorna o primeiro índice em que o elemento pode ser encontrado no array, retorna -1 caso o mesmo não esteja presente.
+// var lista = ['Carlos', 'Natalia', 'Maria', 2023]; // criado uma lista(array) com alguns dados
+// console.log(lista.indexOf('Carlos')); // O método indexOf() retorna o primeiro índice em que o elemento pode ser encontrado no array, retorna -1 caso o mesmo não esteja presente.
 
-console.log(lista.indexOf('Carlos Eduardo')); // retora -1 pois não foi encontrado
+// console.log(lista.indexOf('Carlos Eduardo')); // retora -1 pois não foi encontrado
 
-lista[0] = 'Carlos Eduardo'; //alterado o conteudo da posição 0 que antes era 'Carlos' e agora passou a ser 'Carlos Eduardo' 
+// lista[0] = 'Carlos Eduardo'; // alterado o conteudo da posição 0 que antes era 'Carlos' e agora passou a ser 'Carlos Eduardo' 
 
-console.log(lista.indexOf(2023)); //Usado o metodo indexOf para procurar 2023 no array lista, ele retora a posição 3.
+// console.log(lista.indexOf(2023)); // Usado o metodo indexOf para procurar 2023 no array lista, ele retora a posição 3.
 
+// lista.push('João'); // O método push() adiciona um ou mais elementos ao final de um array e retorna o novo comprimento desse array.
 
+// lista.shift(); // O método *hift() remove o primeiro elemento de um array e retorna esse elemento. Este método muda o tamanho do array.
+// lista.pop(); // O método pop() remove o último elemento de um array e retorna aquele elemento.
 
+// lista.join(', ') // O método join() junta todos os elementos de um array (ou um array-like object) em uma string e retorna esta string. pode ser separado por , . - ; etc...
+
+//###################################################################################################################################################################################
+
+//###################################################################################################################################################################################
+//APRENDENDO LOOPS
+
+// while  =  enquanto
+
+// var x = 0;
+
+// while(x < 10) { // A instrução while cria um loop que executa uma instrução especificada desde que a condição de teste seja avaliada como verdadeira. A condição é avaliada antes de executar a instrução.
+//     document.write("<br> O valor do X é: " + x);
+//     // x++ é igual a expressão x = x + 1;
+//     x++; // O operador de incremento (++) incrementa (adiciona um ao) seu operando e retorna o valor antes ou depois do incremento, dependendo de onde o operador é colocado, exemplo x++(incrementa depois) or ++x(incrementa antes).
+// }
+
+// for = para
+
+// var valor = 15;
+
+// for(a = 0; a <= valor; a++) { // A instrução for cria um loop que consiste em três expressões opcionais, entre parênteses e separadas por ponto e vírgula, seguidas por uma instrução (geralmente uma instrução de bloco) a ser executada no loop.
+//     document.write("<br> O valor do A é: " + a);
+// }
+
+//###################################################################################################################################################################################
+//CONHECENDO SWITCH
+
+// function pedir() {
+//     var valor = prompt('Digite um valor de 1 a 4:');
+//     console.log(typeof valor); // Cuidado pois o que o prompt retorna é uma string e no caso precisamos de um numerico então vamos converter
+//     console.log(typeof Number(valor)); // retonar um number
+
+//     switch(Number(valor)) { // A instrução switch avalia uma expressão, comparando o valor da expressão com uma série de cláusulas case
+//         case 1:
+//             alert("Você escolheu 1 = Suco");
+//         break;
+//         case 2:
+//             alert("Você escolheu 2 = Agua gelada");
+//         break;
+//         case 3:
+//             alert("Você escolheu 3 = Sorvete");
+//         break;
+//         case 4:
+//             alert("Você chamou o garçom!");
+//         break;
+//         default:
+//             alert("Escolha uma opção entre 1 a 4");
+//         break;
+//     }
+
+//     //switch com variavel texto (string)
+//     switch(valor) { // A instrução switch avalia uma expressão, comparando o valor da expressão com uma série de cláusulas case
+//         case 'Carlos':
+//             alert("Olá CARLOS");
+//         break;
+//         case 'Natalia':
+//             alert("Olá NATALIA");
+//         break;
+//         default:
+//             alert("Olá FULANOS");
+//         break;
+//     }
+// }
+
+//###################################################################################################################################################################################
+//MANIPULANDO DATAS
+
+// var data = new Date(); //Criando com a data de hoje do sistema do computador
+// console.log(data);
+
+// Mas e como desmenbrar essa data? pois ela retorna assim: Sat Mar 11 2023 17:31:19 GMT-0300 (Horário Padrão de Brasília) e caso eu queira pegar apenas as horas?
+
+//data.get... retorna varios metodos do objeto Date
+// console.log(data.getDate());
+// console.log(data.getMinutes());
+// console.log(data.getSeconds());
+// console.log(data.getMilliseconds());
+
+// // E se eu quiser criar uma data especifica? eu passando o valor da data?
+
+// var datanova = new Date("March, 10, 2018");
+// console.log(datanova);
+
+// // Como converter uma data em milissegundos?
+// console.log("Datanova em milissegundos: " + Date.parse(datanova));
+
+// // Eu posso tambem criar uma nova data passando os milissegundos:
+// var datamilli = new Date(1520650800000);
+// console.log(datamilli);
+
+// // E como pegar apenas uma parte dessa estrutura data? Sat Mar 11 2023 17:38:21 GMT-0300 (Horário Padrão de Brasília)
+// datanova.getDate(); //pegando o dia
+// datanova.getMonth(); // repara que esse get retorna o valor 2 mesmo estando em Mar é porque no javascript começa-se contar do 0, sendo 0=janeiro 1=fevereiro etc...
+// datanova.getDay(); // retorna 6 que é sabado, começa contando no domingo=0, segunda=1, terça=2, quarta=3, etc...
+// datanova.getFullYear() // retorna 2018
+
+// // Formatar a data Dia/Mês/Ano
+// console.log(datanova.getDate() + "/0" + (datanova.getMonth() + 1) + "/" + datanova.getFullYear());
+
+// // Alterando o data somando +5 no dia de dia 10 para dia 15
+// datanova.setDate(datanova.getDate() + 5);
+
+// // Alterando as horas somando +10 horas
+// datanova.setHours(datanova.getHours() + 10);
+
+// // Como mostrar se hoje é segunda, ternça quarta, quinta, sexta, sabado ou domingo?
+// datanova.getDay();
+// var dias = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sabado"];
+// dias[datanova.getDay()];
+//###################################################################################################################################################################################
+
+//###################################################################################################################################################################################
+//PRATICANDO CONHECIMENTOS
+
+// Criando uma calculadora de IMC
+
+/*
+Abaixo de 17 Muito abaixo do peso
+Entre 17 e 18,49 abaixo do peso
+Entre 18,5 e 24,99 Peso normal
+Entre 25 e 29,99 Acima do peso
+*/
+
+// peso / (altura * altura);
+
+var peso;
+var altura;
+var imc;
+var resultado;
+
+function calcularIMC(event) { // Índice de massa corporal
+    event.preventDefault(); // Cancela o evento se for cancelado, sem parar a propagação do mesmo. nesse caso cancela o recarregamento da pagina pois a programação interpreta como o envio de um formulário e por padrão recarrega.
+    
+    peso = document.getElementById('peso').value; // pegando la do document um elemento pelo seu id, esse id peso foi declarado la no index.html
+    altura = document.getElementById('altura').value;
+    console.log(peso);
+    console.log(altura);
+
+    imc = peso / (altura * altura);
+    console.log(imc);
+
+    resultado = document.getElementById('resultado');
+    if(imc < 17) {
+        resultado.innerHTML = '<br/> Seu resultado foi: ' + imc.toFixed(2) + '<br/> Cuidado você está muito abaixo do peso!'; //O método toFixed() formata um número utilizando notação de ponto fixo.
+    } else if(imc > 17 && imc <= 18.49) {
+        resultado.innerHTML = '<br/> Seu resultado foi: ' + imc.toFixed(2) + '<br/> Você está abaixo do peso!';
+    } else if(imc >= 18.5 && imc <= 24.99) {
+        resultado.innerHTML = '<br/> Seu resultado foi: ' + imc.toFixed(2) + '<br/> Você está no peso ideal!';
+    } else if(imc > 25 && imc <= 29.99) {
+        resultado.innerHTML = '<br/> Seu resultado foi: ' + imc.toFixed(2) + '<br/> Você está acima do peso!';
+    } else if(imc >= 30) {
+        resultado.innerHTML = '<br/> Seu resultado foi: ' + imc.toFixed(2) + '<br/> Cuidado Obsidade!';
+    }
+
+    // Limpando os campos após o calculo
+    document.getElementById('peso').value = '';
+    document.getElementById('altura').value = '';
+}
 //###################################################################################################################################################################################
