4d8792c9905c1a1f2adf66534a64d9c8345cca8b
Carlos Eduardo Correia de Souza <CarlosCorreia1002@gmail.com>
2023-03-14 01:30:34 -0300

    update primeiro-app

--- primeiro-app/src/Anotações.js
+++ primeiro-app/src/Anotações.js
@@ -70,6 +70,104 @@
 //#######################################################################################################################
 
 //#######################################################################################################################
+// MANIPULANDO FORMULARIOS
+
+// import { useState } from "react";
+
+// function App() {
+//   //Declarando os estados
+//   const [nome, setNome] = useState(''); // useState sendo um texto
+//   const [email, setEmail] = useState('');
+//   const [idade, setIdade] = useState('');  
+
+//   const[user, setUser] = useState({}); // useState sendo um objeto
+
+//   // exemplo do useState sendo um array: const[usuarios, setUsuarios] =  useState([]); esse array pode começar preenchido tambem.
+//   // exemplo do useState sendo um numero: const [idade, setIdade] = useState(0); 
+
+//   function handleRegister(e) {
+//     e.preventDefault(); // previne de atualizar o formulario, e os dados sumirem
+    
+//     alert('Usuario registrado com sucesso!');
+//     setUser({ //objeto user
+//       nome: nome,
+//       idade: idade,
+//       email: email
+//     })
+//   }
+
+//   return(
+//     <div>
+//       <h1>Cadastrando usuario</h1>
+
+//       <form onSubmit={handleRegister}>
+//         <label>Nome:</label> <br/>
+//         <input 
+//           placeholder="Digite seu nome" //Mensagem exibida no input quando está vazio
+//           value={nome} //valor que eu tenho dentro do input
+//           // esse "e" dentro da função anonima é abreviação de event
+//           onChange={ (e) => setNome(e.target.value)}
+//         /> <br/>
+
+//         <label>Email:</label> <br/>
+//         <input 
+//           placeholder="Digite seu email"
+//           value={email}
+//           onChange={ (e) => setEmail(e.target.value)}
+//         /> <br/>
+
+//         <label>Idade:</label> <br/>
+//         <input 
+//           placeholder="Digite sua idade"
+//           value={idade}
+//           onChange={ (e) => setIdade(e.target.value)}
+//         /> <br/> <br/>
+        
+//         <button type="submit">Registrar</button>
+//       </form>
+
+//       <br/><br/>
+
+//       <div>
+//         <span>Bem vindo: {user.nome}</span> <br/>
+//         <span>Idade: {user.idade}</span> <br/>
+//         <span>Email: {user.email}</span> <br/>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+//#######################################################################################################################
+
+//#######################################################################################################################
+// PRATICANDO - lista, array e seus estados.
+
+
+
+//#######################################################################################################################
+
+//#######################################################################################################################
+//
+
+//#######################################################################################################################
+
+//#######################################################################################################################
+//
+
+//#######################################################################################################################
+
+//#######################################################################################################################
+//
+
+//#######################################################################################################################
+
+//#######################################################################################################################
+//
+
+//#######################################################################################################################
+
+//#######################################################################################################################
 //
 
 //#######################################################################################################################
